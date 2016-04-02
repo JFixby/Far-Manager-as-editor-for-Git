f1814460d94eb027df235668c56e8e56754e899a
Ruslan Garipov <brigadir15@gmail.com>
2016-04-02 18:36:04 +0500

    Run Far file manager as Git `core.editor`

--- source/farexec/farexec.cpp
+++ source/farexec/farexec.cpp
@@ -3,37 +3,34 @@
 #include <stdio.h>
 #include <Windows.h>
 
-int wmain(int argc, wchar_t* argv[], wchar_t*)
+int wmain()
 {
-    if (2 > argc)
+    LPTSTR p = GetCommandLine();
+    std::wcmatch m {};
+    if (std::regex_match(p, m, std::wregex {LR"(".*"[[:s:]](.*))"}) ||
+        std::regex_match(p, m, std::wregex {LR"(.*?[[:s:]](.*))"}))
     {
-        wprintf_s(L"Usage:\nfarexec.exe {Far executable} {parameters}\n");
-    }
-    else
-    {
-        LPTSTR p = GetCommandLine();
-        std::wcmatch m {};
-        if (std::regex_match(p, m, std::wregex {LR"(".*"[[:s:]](.*))"}) || std::regex_match(p, m, std::wregex {LR"(.*?[[:s:]](.*))"}))
+        const wchar_t farManager[] = L"d:\\far\\far.exe";
+        size_t size = _countof(farManager) + m[1].length() + 1;
+        p = static_cast<LPTSTR> (HeapAlloc(GetProcessHeap(), 0, size * sizeof(TCHAR)));
+        if (p)
         {
-            size_t size = m[1].length() + 1;
-            p = static_cast<LPTSTR> (HeapAlloc(GetProcessHeap(), 0, size * sizeof(TCHAR)));
-            if (p)
+            wcscpy_s(p, size, farManager);
+            wcscat_s(p, size, L" ");
+            wcscat_s(p, size, m[1].str().c_str());
+            STARTUPINFO si {};
+            si.cb = sizeof(STARTUPINFO);
+            si.dwFlags = 0;
+            PROCESS_INFORMATION pi;
+            BOOL result = CreateProcess(
+                farManager, p, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &pi);
+            if (result)
             {
-                wcscpy_s(p, size, m[1].str().c_str());
-                STARTUPINFO si {};
-                si.cb = sizeof(STARTUPINFO);
-                si.dwFlags = 0;
-                PROCESS_INFORMATION pi;
-                BOOL result = CreateProcess(
-                    argv[1], p, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &pi);
-                if (result)
-                {
-                    WaitForSingleObject(pi.hProcess, INFINITE);
-                    CloseHandle(pi.hThread);
-                    CloseHandle(pi.hProcess);
-                }
-                HeapFree(GetProcessHeap(), 0, p);
+                WaitForSingleObject(pi.hProcess, INFINITE);
+                CloseHandle(pi.hThread);
+                CloseHandle(pi.hProcess);
             }
+            HeapFree(GetProcessHeap(), 0, p);
         }
     }
     return 0;
